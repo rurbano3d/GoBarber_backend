48cf91a7f2bcce4e8545b55d0826b25f6c4c0a2e
rurbano3d <rurbano3d@gmail.com>
2019-08-26 11:32:23 -0300

    chore: Appointments,Notifications and Schedule

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,10 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
+import ProviderControlller from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,6 +20,17 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware); // so vale para rotas abaixo dele
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderControlller.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
